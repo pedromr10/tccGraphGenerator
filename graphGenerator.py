27d732f4f0595ec1ff4c7dcc8836b934aafc0cf1
Gustavo Gomes Barbosa <gustbarbr@gmail.com>
2025-10-22 20:52:51 -0300

    Correção

--- graphGenerator.py
+++ graphGenerator.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 #GRAFICO EMOCOES:
 #mostra a quantidade de informacoes retiradas e quantas estavam certas:
 df = pd.DataFrame({
-    'Emotion': ["sad", "angry", "happy", "neutral", "surprised"],
-    'Total emotions': [33, 0, 1, 22, 0],
-    'Correct emotions': [33, 0, 0, 21, 0]
+    'Emotion': ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"],
+    'Total emotions': [0, 0, 0, 0, 0, 0, 0],
+    'Correct emotions': [0, 0, 0, 0, 0, 0, 0]
 })
 #cria um grafico de barras:
-df.plot(x='Emotion', kind='bar', y=['Total emotions', 'Correct emotions'], title='Detected vs. Correctly Detected Emotions')
+df.plot(x='Emotion', kind='bar', y=['Total emotions', 'Correct emotions'], title='Detected vs. Correctly Detected Emotions - Insira o nome aqui')
 #ajuste de layout:
 plt.ylabel('Number of images')
 plt.xticks(rotation=0)
@@ -24,7 +24,7 @@ labels = ['Total object identifications', 'Correct object identifications']
 values = [100, 83] #total / certas
 plt.figure(figsize=(6,4))
 plt.bar(labels, values, color=['skyblue', 'lightgreen'], width=0.4)
-plt.title('Detected vs. Correctly Detected Objects')
+plt.title('Detected vs. Correctly Detected Objects - Insira o nome aqui')
 plt.ylabel('Number of Identifications')
 plt.tight_layout()
 plt.savefig('grafico_identificacao_yolo.png')
